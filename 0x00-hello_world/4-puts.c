a7618ddf820c91575573eb82462883a31825f24f
Ephi123 <ephrembereket36@gmail.com>
2022-09-08 09:42:05 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Sucess)
  */
- int main(void){
- puts("\"Programming is like building a multilingual puzzle\n");
+ int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle ");
  return (0);
  
  }
