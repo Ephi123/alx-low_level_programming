53a16a364ec3af9e1d5ebc105201be03101b0a6f
Ephi123 <ephrembereket36@gmail.com>
2022-09-20 08:10:06 -0700

    yap

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generate random valid passwords
+ * Return: 0 (always)
+ */
+int main(void)
+{
+int pass, sum;
+srand(time(NULL));
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum += pass;
+printf("%c", 2772 - sum);
+return (0);
+}
+}
