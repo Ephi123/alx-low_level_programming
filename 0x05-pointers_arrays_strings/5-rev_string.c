2d2d514fd2c9fa806ac9ffccac66827c3b989918
Ephi123 <ephrembereket36@gmail.com>
2022-09-19 18:28:36 -0700

    give your best

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string -> reverese
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+int len;
+int len2;
+int len = 0;
+int i;
+for (; s[len] != '\0'; len++)
+{
+len2 = len;
+}
+for (i = 0; i <= len; len--)
+{
+_putchar(s[len]);
+}
+putchar('\n');
+}
