391d1fa4d5d3da97e1acbd0f501d99de9ab9bc7e
Ephi123 <ephrembereket36@gmail.com>
2022-09-19 19:14:58 -0700

    not clear

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,23 @@
 #include "main.h"
-#include <string.h>
 /**
  * rev_string -> reverese
  * @s: parameter
  */
 void rev_string(char *s)
 {
-int len;
-int i;
-len = strlen(s);
-for (i = 0; i <= len; len--)
+char temp;
+int len1, len2, i;
+len1 = 0;
+len2 = 0;
+
+while (s[len1] != '\0')
+len1++
+len2 = len1 - 1;
+for (i = 0; i <= len1 / 2; len1++)
 {
-_putchar(s[len]);
-s[len] = '\0';
+temp = s[i];
+s[i] = s[len2];
+s[len2] = temp;
+len2 - = 1;
 }
-_putchar('\n');
 }
