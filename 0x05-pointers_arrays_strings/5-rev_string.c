a10b8d44a9bd36ebae43857e4712a4b887883977
Ephi123 <ephrembereket36@gmail.com>
2022-09-19 18:40:01 -0700

    give your best

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,11 @@ int len2;
 int i;
 for (len = 0; s[len] != '\0'; len++)
 {
+_putchar(s[len]);
 len2 = len;
 }
-for (i = 0; i <= len2-1; len2--)
+_putchar('\n');
+for (i = 0; i <= len2; len2--)
 {
 _putchar(s[len2]);
 }
