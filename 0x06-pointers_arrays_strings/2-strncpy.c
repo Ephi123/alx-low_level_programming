0f699ac7404f7725e1d311e8c4e31bf07e7665f9
Ephi123 <ephrembereket36@gmail.com>
2022-09-22 13:46:18 -0700

    fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-int j=0;
+int j = 0;
 while (src[i] != '\0')
 {
 i++;
 }
-while ((dest[j] != '\0') && (j < n)){
+while ((dest[j] != '\0') && (j < n))
+{
 dest[j] = src[j];
 j++;
 }
-for( j = i; j < n; j++)
+for (j = i; j < n; j++)
 dest[j] = '\0';
 return (dest);
 }
