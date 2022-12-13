14c6aefb6345e7115e626eb0b736bc99429857d5
Ephi123 <99554853+Ephi123@users.noreply.github.com>
2022-12-13 03:25:24 +0300

    advance

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,42 @@
+/**
+ *add - perform adition
+ *
+ * Return: a + b
+ */
+int add(a, b)
+  return a + b;
+
+/**
+ *sub - perform subtraction
+ *
+ * Return: a - b
+ */
+int sub(a, b)
+  return a - b;
+
+/**
+ *mul - perform multiplication
+ *
+ * Return: a - b
+ */
+int mul(a, b)
+  return a * b;
+
+/**
+ *div - perform div
+ *
+ * Return: a / b
+ */
+int div(a, b)
+  return a / b;
+
+/**
+ *mod - perform mod
+ *
+ * Return: a - b
+ */
+int mod(a, b)
+  return a % b;
+  
+
+  
