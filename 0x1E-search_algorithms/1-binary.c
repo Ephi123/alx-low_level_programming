a57a7f766e71eb67b9d65aa63136582382b97ab3
Ephi123 <99554853+Ephi123@users.noreply.github.com>
2023-04-16 02:34:19 +0300

    try to fix binary search algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
+ * binary_search - searches for a value in an array of
  * integers using the Binary search algorithm
  *
  *
@@ -10,52 +10,29 @@
  * @value: value to search in
  * Return: index of the number
  */
-int recursive_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+	int left = 0;
+	int right = size - 1;
+	int mid, i;
 
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+		}
+		printf("\n");
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return (-1);
 }
